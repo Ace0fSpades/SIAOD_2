e667038eb93928bb86c6920e0afc90845696425e
Splesh <spleshnightcore@gmail.com>
2021-10-04 20:47:02 +0300

    final

--- Hash/Hash.cpp
+++ Hash/Hash.cpp
@@ -3,24 +3,24 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <ctime>
+#include <chrono>
 
 using namespace std;
 
-void generate_file()
+void generate_file(int count = 100)
 {
     int length = 7; // определяем длину кода специализации
     char line[16];
     FILE* fout = fopen("text.bin", "wb"); // создаем файл для записи в бинарном виде
 
-    for (int j = 0; j < 100; j++) {
+    for (int j = 0; j < count; j++) {
         for (int i = 0; i < length; i++)
             line[i] = char(rand() % 9 + 48);
         line[7] = ' ';
         length = 15; // определяем длину названия вуза
         for (int i = 8; i < length; i++)
             line[i] = char(rand() % 25 + 65);
-        if (j == 99) line[15] = '\0';
+        if (j == count-1) line[15] = '\0';
         else line[15] = '\n';
         fwrite(line, 16, 1, fout);        
     }
@@ -32,24 +32,20 @@ struct Stack {
     string code;
     string name;
     Stack* Next;
-    Stack* Head;
-    
+    Stack* Head;    
 
     Stack() {
         code = "";
         name = "";
         Next = nullptr;  
-        Head = this;
-        
-        
+        Head = this;        
     }
+
     Stack(string code, string name) {
         this->code = code;
         this->name = name;
         Next = nullptr;
-        Head = this;
-        
-        
+        Head = this;       
     }
 
     bool empty() {
@@ -60,27 +56,22 @@ struct Stack {
         Stack* a = new Stack(code, name);
         if (empty()) {
             Head = a;
-            Next = a->Next;
-            
+            Next = a->Next;            
             return;
         }
         Stack* tmp;
         tmp = this;
         while(tmp->Next != NULL)
         tmp = tmp->Next;        
-        tmp->Next = a;
-        
+        tmp->Next = a;        
         a->Head = this;
     }
-
     };
 
 class Hash_table {
 private:
-    Stack* table[100];   
-
+    Stack* table[100];
 public:
-
     Hash_table() {
         for (int i = 0; i < 100; i++) {            
             table[i] = new Stack();
@@ -91,14 +82,12 @@ public:
             delete table[i];
         }
     }
-
     int hash(string code);
     void read_paste();    
     void delete_target(string code);
     void find(string code);
     void insert_target(string code, string name);
     void table_out();
-
 };
 
 
@@ -111,7 +100,7 @@ int Hash_table::hash(string code)
         result += int(code[i]);
         i++;
     }    
-    return ((result % 256)%100);
+    return ((result * 123 )%100);
 }
 
 void Hash_table::read_paste()
@@ -188,16 +177,19 @@ void Hash_table::insert_target(string code, string name) {
 
 void Hash_table::find(string code)
 {
+    auto begin = std::chrono::steady_clock::now();
+
     int index = hash(code);    
     Stack* tmp = table[index];    
    while (tmp != NULL && tmp->code != code) {
             tmp = tmp->Next;
-   } 
+   }
+   auto end = std::chrono::steady_clock::now();
+   auto elapsed_ms = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
    if (tmp)
-       cout << "Hash: " << index << " | " << tmp->code << " | " << tmp->name << '\n';
-   else cout << "Hash not found.\n";   
-    
-
+       cout << "Hash: " << index << " | " << tmp->code << " | " << tmp->name << '\n' << "Elapsed time: "<< elapsed_ms.count() << "ns\n";
+   
+   else cout << "Hash not found.\n" << "Elapsed time: " << elapsed_ms.count() << "ns\n";
 }
 
 void Hash_table::table_out()
@@ -217,7 +209,7 @@ void Hash_table::table_out()
 
 int main()
 {
-    generate_file();
+    generate_file(1000);
     
     Hash_table test;
     test.read_paste();
@@ -230,7 +222,8 @@ int main()
         case 2: {
             string code;
             cout << "Insert code: ";
-            cin >> code;            
+            cin >> code;
+            system("cls");
             test.find(code);
             system("pause");
             system("cls");
